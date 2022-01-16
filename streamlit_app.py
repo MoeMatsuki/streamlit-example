b5f73895587abdf7c7a784902513ea6cbb15bd58
MoeMatsuki <matsuki.sousisu@gmail.com>
2022-01-17 01:33:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# -*- coding: utf-8 -*-
+
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import pandas_profiling
+import pickle5
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main():
+    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+    pklfile_path = "data/202111_catch_tissuepaper/2021-11-14-17-28-07-129_0.pkl"
+    with open(pklfile_path, "rb") as fh:
+            df =  pickle5.load(fh)
+    pr = df.profile_report()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    st_profile_report(pr)
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+if __name__ == '__main__':
+    main()
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
