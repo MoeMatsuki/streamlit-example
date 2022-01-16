36ee619165951cee3b8d320d53de1d435cd87adf
MoeMatsuki <matsuki.sousisu@gmail.com>
2022-01-17 01:35:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,27 @@
-# -*- coding: utf-8 -*-
-
+import altair as alt
 import streamlit as st
 import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
-import pandas_profiling
-import pickle5
+import numpy as np
 
+from streamlit_vega_lite import vega_lite_component, altair_component
 
-def main():
-    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
-    pklfile_path = "data/202111_catch_tissuepaper/2021-11-14-17-28-07-129_0.pkl"
-    with open(pklfile_path, "rb") as fh:
-            df =  pickle5.load(fh)
-    pr = df.profile_report()
+hist_data = pd.DataFrame(np.random.normal(42, 10, (200, 1)), columns=["x"])
 
-    st_profile_report(pr)
+@st.cache
+def altair_histogram():
+    brushed = alt.selection_interval(encodings=["x"], name="brushed")
 
+    return (
+        alt.Chart(hist_data)
+        .mark_bar()
+        .encode(alt.X("x:Q", bin=True), y="count()")
+        .add_selection(brushed)
+    )
 
-if __name__ == '__main__':
-    main()
+event_dict = altair_component(altair_chart=altair_histogram())
+
+r = event_dict.get("x")
+vega_lite_component()
+if r:
+    filtered = hist_data[(hist_data.x >= r[0]) & (hist_data.x < r[1])]
+    st.write(filtered)
